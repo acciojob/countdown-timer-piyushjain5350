2f12e22ae37f5be3438dc9fcca14e7ac5a5ad155
Piyush Jain <126599802+piyushjain5350@users.noreply.github.com>
2023-10-07 21:39:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // Your script here.
+const countDownDisplay=document.querySelector("#countDown");
+const endTimeDisplay=document.querySelector("#endTime");
+
+document.querySelector("button").addEventListener("click",()=>{
+    let countDownTime=document.querySelector("input").value;
+    let currDate=new Date();
+    let endTime=addMinutes(currDate,Number(countDownTime));
+    console.log(endTime);
+    countDownDisplay.textContent=currDate.toLocaleTimeString();
+    endTimeDisplay.textContent=formatEndTime(endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+    endTime=endTime.toLocaleTimeString();
+    let displayTime = setInterval(()=>{
+        let currTime = new Date().toLocaleTimeString();
+        countDownDisplay.textContent=currTime;
+        if( currTime==endTime) clearInterval(displayTime);
+    },1000)
+})
+
+const addMinutes=(date,mins)=>{
+    date.setMinutes(date.getMinutes()+mins);
+    return date;
+}
+
+const formatEndTime=(timeString)=>{
+    const [hours, minutes, meridiem] = timeString.split(/:| /);
+    const formattedTimeString = `${parseInt(hours, 10)}:${minutes} ${meridiem}`;
+    return formattedTimeString;
+}
